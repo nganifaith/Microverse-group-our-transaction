369fbbacb983c4b9ef301776a9f7e936da97885d
ngani faith <nganifaith@gmail.com>
2021-04-27 20:33:14 +0100

    Fix rubocop errors

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Transaction, type: :model do
 
   it "doesn't create a transaction with an empty name, amount or description" do
     @transaction2 = Transaction.create(name: '',
-                                        description: '',
-                                        amount: nil,
-                                        user_id: @user.id)
+                                       description: '',
+                                       amount: nil,
+                                       user_id: @user.id)
     expect(@transaction2.valid?).to eq false
   end
 
   it 'has to belong to a user' do
-    @transaction1 = Transaction.new(name: 'Transaction01', 
+    @transaction1 = Transaction.new(name: 'Transaction01',
                                     description: 'description for trans 01',
                                     amount: 0.50)
     expect(@transaction1.valid?).to eq false
