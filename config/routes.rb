ad29aeb6c8bbc57587ac49a5a21d2919f253fc27
ngani faith <nganifaith@gmail.com>
2021-05-02 22:17:56 +0100

    Add routes and add bootstrap class to sessions html

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'staticpages#home'
   get 'transaction/external'
   devise_for :users
-  resources :users, only: %[index show]
-  resources :groups
-  resources :transactions
+  resources :groups, only: [:index, :show, :new, :create]
+  resources :transactions, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
